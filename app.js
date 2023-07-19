1c17273eec338a99f943900d4e04eec256c79378
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-07-19 20:25:23 +0100

    Create new tour established using POST request.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+// express.json is middleware
 
 // .get is the http method here.
 // a callback function is the secodn argument
@@ -23,6 +25,12 @@ app.get("/api/v1/tours", (req, res) => {
   res
     .status(200)
     .json({ status: "Sucess", results: tours.length, data: { tours } });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId, req.body});
+  res.send("done"); //we alwyas need to send back something
 });
 
 const port = 3001;
