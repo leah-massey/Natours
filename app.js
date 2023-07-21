b7f68131ea8add208d4d3add0b59c0d70b4b66f5
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-07-21 10:18:01 +0100

    server folder created and port info moved from app.j. Export and import of app set up. Pakcage.json updated : npm test becomes npm start with .

--- app.js
+++ app.js
@@ -24,7 +24,4 @@ app.use((req, res, next) => {
 app.use("/api/v1/tours", tourRouter); //tourRouter is middleware
 app.use("/api/v1/users", userRouter);
 
-const port = 3001;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+module.exports = app;
