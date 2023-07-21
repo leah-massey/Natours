8e34a0c605691298de02552bdf085e36ec7bc76a
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-07-21 15:06:06 +0100

    Port updated in server.js to include process.env.PORT.

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = require("./app");
 
 dotenv.config({ path: "./config.env" });
 
-console.log(process.env);
+// console.log(process.env);
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
